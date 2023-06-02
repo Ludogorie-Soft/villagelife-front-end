94191df1d253d94723fc17055c20ee2353b74912
diddka <danailova_rz@abv.bg>
2023-06-02 20:55:24 +0300

    setUp_front_end_api

---
+++ src/main/java/com/ludogorieSoft/villagelifefrontend/VillagelifeFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.ludogorieSoft.villagelifefrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillagelifeFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VillagelifeFrontEndApplication.class, args);
+	}
+
+}
