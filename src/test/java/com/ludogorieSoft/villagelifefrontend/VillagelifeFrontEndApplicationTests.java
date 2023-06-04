7cb6609266c4c0d15e27d47e8cc704dbca8ada96
diddka <danailova_rz@abv.bg>
2023-06-04 17:42:30 +0300

    test setup front end

---
+++ src/test/java/com/ludogorieSoft/villagelifefrontend/VillagelifeFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ludogorieSoft.villagelifefrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillagelifeFrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
