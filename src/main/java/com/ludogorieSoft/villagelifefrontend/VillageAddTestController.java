1e585cabf0258a19ef2991392ec57328b081fab5
Berkant <berki9@icloud.com>
2023-06-11 22:00:07 +0300

    made Village HomePage ready to add List<Villages> whit name and image to showing all villages in DB.

--- src/main/java/com/ludogorieSoft/villagelifefrontend/VillageAddTestController.java
+++ src/main/java/com/ludogorieSoft/villagelifefrontend/VillageAddTestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
-
+import javax.swing.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,28 +22,43 @@ import static com.ludogorieSoft.villagelifefrontend.enums.Residents.From6To10Per
 @Controller
 @RequestMapping("village")
 public class VillageAddTestController {
-    private final Population population=new Population(1L,From11To50People,From6To10Percent,Over50Years,IDontKnow);
-    private final Administrator administrator=new Administrator(1L,"TestAdmin","mail@bg.com","user","pass","80991247128",LocalDateTime.of(2,1,5,1,1,31));
 
-
+    private final Population population = new Population(1L, From11To50People, From6To10Percent, Over50Years, IDontKnow);
+    private final Administrator administrator = new Administrator(1L, "TestAdmin", "mail@bg.com", "user", "pass", "80991247128", LocalDateTime.of(2, 1, 5, 1, 1, 31));
+    private final Village village = new Village(2L,new ImageIcon("/images/village-live-logo-circle.png") ,"SELO 1", population, LocalDateTime.of(2, 1, 5, 1, 1, 31), true, administrator, LocalDateTime.of(1, 2, 3, 4, 5, 6, 7));
+    private final Village village2 = new Village(3L,new ImageIcon("/images/village-live-logo-circle.png"), "SELO 2 ", population, LocalDateTime.of(2, 1, 5, 1, 1, 31), true, administrator, LocalDateTime.of(1, 2, 3, 4, 5, 6, 7));
+    private final Village village3 = new Village(4L,new ImageIcon("/images/village-live-logo-circle.png"), "SELO 3", population, LocalDateTime.of(2, 1, 5, 1, 1, 31), true, administrator, LocalDateTime.of(1, 2, 3, 4, 5, 6, 7));
+    private final Village village4 = new Village(5L,new ImageIcon("/images/village-live-logo-circle.png"), "SELO 4", population, LocalDateTime.of(2, 1, 5, 1, 1, 31), true, administrator, LocalDateTime.of(1, 2, 3, 4, 5, 6, 7));
     @GetMapping("/add")
     //http://localhost:8087/village/add
     public String addVillage(Model model) {
-        model.addAttribute("village",new Village());
+        model.addAttribute("village", new Village());
         return "Village";
     }
 
-    @GetMapping ("/show")
+    @GetMapping("/show")
     //http://localhost:8087/village/show
-    public String stringVillage( Model model) {
-        Village village=new Village(2L,"SELO1",population, LocalDateTime.of(2,1,5,1,1,31),true,administrator,LocalDateTime.of(1,2,3,4,5,6,7));
-        Village village2=new Village(2L,"SELO2",population, LocalDateTime.of(2,1,5,1,1,31),true,administrator,LocalDateTime.of(1,2,3,4,5,6,7));
-
-        List<Village> villageList=new ArrayList<>();
+    public String stringVillage(Model model) {
+        List<Village> villageList = new ArrayList<>();
         villageList.add(village);
         villageList.add(village2);
-        model.addAttribute("villages",villageList);
-        return "Show";
+        villageList.add(village3);
+        villageList.add(village4);
+
+        model.addAttribute("villages", villageList);
+        return "test";
+    }
+
+    @GetMapping("/home-page")
+    //http://localhost:8087/village/home-page
+    public String homePage(Model model) {
+        List<Village> villageList = new ArrayList<>();
+        villageList.add(village);
+        villageList.add(village2);
+        villageList.add(village3);
+        villageList.add(village4);
+        model.addAttribute("villages", villageList);
+        return "HomePage";
     }
 
 
--- src/main/java/com/ludogorieSoft/villagelifefrontend/models/Village.java
+++ src/main/java/com/ludogorieSoft/villagelifefrontend/models/Village.java
@@ -4,16 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.swing.*;
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Village {
-
     private Long id;
+    private ImageIcon image;
     private String name;
-
     private Population population;
     private LocalDateTime dateUpload;
     private boolean status;
