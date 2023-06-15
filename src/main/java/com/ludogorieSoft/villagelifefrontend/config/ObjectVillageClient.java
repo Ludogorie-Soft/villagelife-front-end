c7c15adcc9dcbd68d32b29b81d44bd1c92bd344c
Berkant <berki9@icloud.com>
2023-06-15 21:18:46 +0300

    fix show village by id in project VillageLiveFrontEnd добавяне на таблица с Обекти на съответните застояния от селото

---
+++ src/main/java/com/ludogorieSoft/villagelifefrontend/config/ObjectVillageClient.java
@@ -0,0 +1,19 @@
+package com.ludogoriesoft.villagelifefrontend.config;
+
+import com.ludogoriesoft.villagelifefrontend.dtos.ObjectVillageDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "villagelife-api-object-village-client",url = "http://localhost:8088/api/v1/objectVillages")
+
+public interface ObjectVillageClient {
+    @GetMapping
+   List<ObjectVillageDTO> getAllObjectVillages();
+
+    @GetMapping("/village/{id}")
+    public List<ObjectVillageDTO> getObjectVillageByVillageID(@PathVariable("id") Long id) ;
+
+}
