81820b71eb4d9df9ba63252db34e0f0ffe3bf50d
Nadezhda Todorova <ntodor358@gmail.com>
2023-06-27 00:10:25 +0300

    change the visualization of the villages

--- src/main/java/com/ludogorieSoft/villagelifefrontend/config/VillageImageClient.java
+++ src/main/java/com/ludogorieSoft/villagelifefrontend/config/VillageImageClient.java
@@ -1,7 +1,6 @@
 package com.ludogoriesoft.villagelifefrontend.config;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.multipart.MultipartFile;
