e229e899d4fef11884f944a0a76b342ed156338c
diddka <danailova_rz@abv.bg>
2023-06-26 17:21:23 +0300

    CRUD admin - but update doesn`t work

---
+++ src/main/java/com/ludogorieSoft/villagelifefrontend/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.ludogoriesoft.villagelifefrontend.exceptions;
+
+import feign.FeignException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(FeignException.class)
+    public String handleFeignException(FeignException ex, RedirectAttributes redirectAttributes) {
+        String redirecting = "redirect:/auth/login";
+        String message = "message";
+        if (ex.status() == HttpStatus.FORBIDDEN.value() ) {
+            redirectAttributes.addFlashAttribute(message, "Wrong password or username! Please try again!");
+            return redirecting;
+        } else if (ex.status() == HttpStatus.UNAUTHORIZED.value() ) {
+            redirectAttributes.addFlashAttribute(message, "Your session has expired! Please sign in again!");
+            return redirecting;
+        }else if (ex.status() == HttpStatus.INTERNAL_SERVER_ERROR.value() ) {
+            redirectAttributes.addFlashAttribute(message, "You have to sign in!");
+            return redirecting;
+        }else if (ex.status() == HttpStatus.BAD_REQUEST.value() ) {
+            redirectAttributes.addFlashAttribute(message, "Administrator already exists!");
+            return "redirect:/admins/edit/";
+        }else {
+            redirectAttributes.addFlashAttribute(message, "Something went wrong! Please try again!");
+            return redirecting;
+        }
+    }
+}
