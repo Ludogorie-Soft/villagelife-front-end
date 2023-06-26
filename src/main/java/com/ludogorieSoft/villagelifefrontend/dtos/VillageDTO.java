41c368849408968ebabfb367ad72ebcfacf41826
Berkant <berki9@icloud.com>
2023-06-26 03:04:08 +0300

    merge excel_file_upload in add_filter_search

--- src/main/java/com/ludogorieSoft/villagelifefrontend/dtos/VillageDTO.java
+++ src/main/java/com/ludogorieSoft/villagelifefrontend/dtos/VillageDTO.java
@@ -1,5 +1,6 @@
 package com.ludogoriesoft.villagelifefrontend.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class VillageDTO {
 
     private Long id;
