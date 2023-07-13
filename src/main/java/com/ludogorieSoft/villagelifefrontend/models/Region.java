da72aadb10c90f3238bddf189d55734303f61c11
diddka <danailova_rz@abv.bg>
2023-07-13 20:33:59 +0300

    admin show villages

---
+++ src/main/java/com/ludogorieSoft/villagelifefrontend/models/Region.java
@@ -0,0 +1,17 @@
+package com.ludogorieSoft.villagelifefrontend.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Region {
+
+    private Long id;
+
+    private String regionName;
+}
