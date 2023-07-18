f1956f47fd9e8452f9165eb4c711683099d36a4e
Nadezhda Todorova <ntodor358@gmail.com>
2023-07-18 16:11:13 +0300

    uncomment AdministratorControllerTest and remove code in comment in VillageController

--- src/main/java/com/ludogorieSoft/villagelifefrontend/controllers/VillageController.java
+++ src/main/java/com/ludogorieSoft/villagelifefrontend/controllers/VillageController.java
@@ -41,14 +41,6 @@ public class VillageController {
         model.addAttribute("villages", villages);
         return "/test/testAllVillages";
     }
-    //@GetMapping("/home-page")
-    //public String homePage(Model model) {
-    //    List<RegionDTO> regionDTOS = regionClient.getAllRegions();
-    //    model.addAttribute("regions", regionDTOS);
-    //    List<VillageImageResponse> villageImageResponses = villageImageClient.getAllVillageImageResponses().getBody();
-    //    model.addAttribute("villageImageResponses", villageImageResponses);
-    //    return "HomePage";
-    //}
     @GetMapping("/home-page")
     public String homePage(Model model) {
         List<RegionDTO> regionDTOS = regionClient.getAllRegions();
