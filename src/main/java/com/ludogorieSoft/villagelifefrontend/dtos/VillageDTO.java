52a7cb61e75a64a9ab34f0e54135de56088a8e1c
Nadezhda Todorova <ntodor358@gmail.com>
2023-08-01 01:47:39 +0300

    change dateUpload type to LocalDateTime

--- src/main/java/com/ludogorieSoft/villagelifefrontend/dtos/VillageDTO.java
+++ src/main/java/com/ludogorieSoft/villagelifefrontend/dtos/VillageDTO.java
@@ -1,14 +1,18 @@
 package com.ludogorieSoft.villagelifefrontend.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.ludogorieSoft.villagelifefrontend.models.Population;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
@@ -23,8 +27,10 @@ public class VillageDTO {
     private String region;
     private int populationCount;
     private PopulationDTO populationDTO;
-    @DateTimeFormat(pattern = "yyyy-mm-dd")
-    private Date dateUpload;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    private LocalDateTime dateUpload;
     private boolean status;
     private List<String> images;
 
