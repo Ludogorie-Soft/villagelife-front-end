d062f4c2d7f4a89ff1081e0da0ea704b83d566af
Nadezhda Todorova <ntodor358@gmail.com>
2023-09-06 22:48:25 +0300

    edit imageSlider.js

--- src/main/resources/static/js/imageSlider.js
+++ src/main/resources/static/js/imageSlider.js
@@ -1,23 +1,25 @@
-    let slideIndex = 1;
-    showSlides(slideIndex);
+let slideIndex = 1;
+showSlides(slideIndex);
 
-    function plusSlides(n) {
-      showSlides(slideIndex += n);
-    }
+setInterval(function() {
+  plusSlides(1);
+}, 10000);
 
-    function currentSlide(n) {
-      showSlides(slideIndex = n);
-    }
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-    function showSlides(n) {
-      let i;
-      let slides = document.getElementsByClassName("mySlides");
-      let captionText = document.getElementById("caption");
-      if (n > slides.length) {slideIndex = 1}
-      if (n < 1) {slideIndex = slides.length}
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-      }
-      slides[slideIndex-1].style.display = "block";
-      captionText.innerHTML = "";
-    }
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
