2f00c7dfb80d6bd7990feb432c98cbfecca4db66
diddka <danailova_rz@abv.bg>
2024-02-21 22:38:40 +0200

    add locale

--- src/main/java/com/ludogorieSoft/villagelifefrontend/controllers/LocaleController.java
+++ src/main/java/com/ludogorieSoft/villagelifefrontend/controllers/LocaleController.java
@@ -1,18 +1,34 @@
 package com.ludogorieSoft.villagelifefrontend.controllers;
 
+import com.ludogorieSoft.villagelifefrontend.localization.MyLocaleResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
-@RestController
+@Controller
 @RequestMapping("/locale")
 public class LocaleController {
+    @Autowired
+    private MessageSource messageSource;
 
+    @Autowired
+    private MyLocaleResolver myLocaleResolver;
 //    @GetMapping
 //    public String getCurrentLocale(HttpServletRequest request) {
 //        System.out.println("request.getLocale().toString()   " + request.getLocale().toString());
 //        return request.getLocale().toString();
+////        return messageSource.getMessage("hello", null, myLocaleResolver.resolveLocale(request));
 //    }
+
+    @GetMapping
+    public String getInternationalPage() {
+        return "hello";
+    }
+
 }
---
+++ src/main/java/com/ludogorieSoft/villagelifefrontend/localization/MyLocaleResolver.java
@@ -0,0 +1,56 @@
+package com.ludogorieSoft.villagelifefrontend.localization;
+
+;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+import java.util.Locale;
+
+@Component
+public class MyLocaleResolver implements WebMvcConfigurer {
+
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.forLanguageTag("bg"));
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+    @Bean
+    public ViewResolver viewResolver(SpringTemplateEngine templateEngine) {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine);
+        resolver.setCharacterEncoding("UTF-8");
+        return resolver;
+    }
+
+}
