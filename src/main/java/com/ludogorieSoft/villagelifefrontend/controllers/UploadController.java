db2deb0dc7cbcc7f714dee346f5c701af939ddac
diddka <danailova_rz@abv.bg>
2024-03-11 21:42:23 +0200

    change upload

--- src/main/java/com/ludogorieSoft/villagelifefrontend/controllers/UploadController.java
+++ src/main/java/com/ludogorieSoft/villagelifefrontend/controllers/UploadController.java
@@ -122,7 +122,6 @@ public class UploadController {
                 for (int i = 0; i < lastCellNum; i++) {
                     cell = row.getCell(i);
                     if (cell != null) {
-                        System.out.println("sellll " + cell);
                         String value = cell.getStringCellValue();
                         village.setId(village.getId());
                         village.setDateUpload(LocalDateTime.now());
@@ -258,41 +257,9 @@ public class UploadController {
                             population.setVillageId(village.getId());
                             population.setStatus(true);
                             boolean populationFound = false;
-
-//                            for (NumberOfPopulation numberOfPopulation : NumberOfPopulation.values()) {
-//                                if (numberOfPopulation.getName().equalsIgnoreCase(valueNumberOfPopulation)) {
-//                                    population.setNumberOfPopulation(numberOfPopulation);
-//                                    String[] populationRange = valueNumberOfPopulation.split(" - ");
-//                                    if (populationRange.length >= 2) {
-//                                        String numberString = populationRange[1].trim().split(" ")[0];
+                            System.out.println("population " + valueNumberOfPopulation);
 
                                     populationFound = isPopulationFound(population, populationFound, valueNumberOfPopulation);//numberString
-//                                    }
-//                                    break;
-//                                }
-
-
-
-
-////                            for (NumberOfPopulation numberOfPopulation : NumberOfPopulation.values()) {
-////                                if (numberOfPopulation.getName().equalsIgnoreCase(valueNumberOfPopulation)) {
-////                                    population.setNumberOfPopulation(numberOfPopulation);
-//                                    String[] populationRange = valueNumberOfPopulation.split("-");;//valueNumberOfPopulation.split(" - ");
-////                                    if (populationRange.length >= 2) {
-//                                        String numberString = populationRange[1].trim();// populationRange[1].trim().split(" ")[0]
-//                            System.out.println("number   " + numberString);
-//                                        populationFound = isPopulationFound(population, populationFound, numberString);
-////                                    }
-////                                    break;
-////                                }
-////                            }
-                            if (!populationFound) {
-                                if (valueNumberOfPopulation.equalsIgnoreCase("над 2000 човека")) {
-                                    population.setPopulationCount(2000);
-                                } else if (valueNumberOfPopulation.equalsIgnoreCase("до 10 човека")) {
-                                    population.setPopulationCount(10);
-                                }
-                            }
                             i++;
                             String valueResident = sheet.getRow(rowIndex).getCell(i).getStringCellValue();
                             for (Residents residents : Residents.values()) {
