326d0e7b9b9808a38aeef5e721e1d87181246901
Nadezhda Todorova <ntodor358@gmail.com>
2024-03-16 16:33:33 +0200

    translate InquiryType

--- src/main/java/com/ludogorieSoft/villagelifefrontend/enums/InquiryType.java
+++ src/main/java/com/ludogorieSoft/villagelifefrontend/enums/InquiryType.java
@@ -2,17 +2,17 @@ package com.ludogorieSoft.villagelifefrontend.enums;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.apache.catalina.LifecycleState;
-
-import javax.persistence.criteria.CriteriaBuilder;
-import java.util.List;
 
 @Getter
 @AllArgsConstructor
 public enum InquiryType {
-    REQUEST_FOR_CONTACT ("Запитване за контакт в село"),
+    /*REQUEST_FOR_CONTACT ("Запитване за контакт в село"),
     LOOKING_FOR_BETTER_INDICATORS("Търся село с по-добри показатели"),
     JOB_OPPORTUNITIES("Възможности за работа"),
-    OTHER("Друго");
+    OTHER("Друго");*/
+    REQUEST_FOR_CONTACT ("inquiryType.request_for_contact"),
+    LOOKING_FOR_BETTER_INDICATORS("inquiryType.looking_for_better_indicators"),
+    JOB_OPPORTUNITIES("inquiryType.job_opportunities"),
+    OTHER("inquiryType.other");
     private final String name;
 }
