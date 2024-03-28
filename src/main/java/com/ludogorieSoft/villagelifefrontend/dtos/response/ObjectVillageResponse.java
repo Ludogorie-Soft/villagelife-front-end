d92079ec6cec9111484d6adfa0a660486e7b534d
Nadezhda Todorova <ntodor358@gmail.com>
2024-03-28 22:53:23 +0200

    translate texts in ShowVillageById.html

--- src/main/java/com/ludogorieSoft/villagelifefrontend/dtos/response/ObjectVillageResponse.java
+++ src/main/java/com/ludogorieSoft/villagelifefrontend/dtos/response/ObjectVillageResponse.java
@@ -1,10 +1,13 @@
 package com.ludogorieSoft.villagelifefrontend.dtos.response;
 
+import com.ludogorieSoft.villagelifefrontend.dtos.ObjectAroundVillageDTO;
 import com.ludogorieSoft.villagelifefrontend.enums.Distance;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -12,5 +15,6 @@ import lombok.Setter;
 @NoArgsConstructor
 public class ObjectVillageResponse {
     private Distance distance;
-    private String objects;
+    /*private String objects;*/
+    private List<ObjectAroundVillageDTO> objects;
 }
