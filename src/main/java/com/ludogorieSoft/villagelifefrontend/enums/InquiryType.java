94fe9bd1b797e30a521bbc138e6d58cefb4dd485
Nadezhda Todorova <ntodor358@gmail.com>
2024-03-30 19:20:16 +0200

    fix Consents

--- src/main/java/com/ludogorieSoft/villagelifefrontend/enums/InquiryType.java
+++ src/main/java/com/ludogorieSoft/villagelifefrontend/enums/InquiryType.java
@@ -6,10 +6,6 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum InquiryType {
-    /*REQUEST_FOR_CONTACT ("Запитване за контакт в село"),
-    LOOKING_FOR_BETTER_INDICATORS("Търся село с по-добри показатели"),
-    JOB_OPPORTUNITIES("Възможности за работа"),
-    OTHER("Друго");*/
     REQUEST_FOR_CONTACT ("inquiryType.request_for_contact"),
     LOOKING_FOR_BETTER_INDICATORS("inquiryType.looking_for_better_indicators"),
     JOB_OPPORTUNITIES("inquiryType.job_opportunities"),
