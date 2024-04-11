52450f2888a8b1a23ed0d050d84686de48eb988a
diddka <danailova_rz@abv.bg>
2024-04-11 20:05:42 +0300

    add village name and region validations

--- src/main/resources/static/js/nameAndRegion.js
+++ src/main/resources/static/js/nameAndRegion.js
@@ -1,51 +1,26 @@
-//function validateForm() {
-//    var isVillageNameValid = validateVillageName();
-//    var isRegionValid = validateRegion();
-//
-//    if (!isRegionValid.value ) {
-//            document.getElementById('regionName-error').style.display = 'block';
-//
-//        return false;
-//    }
-//    if ( !isVillageNameValid) {
-//            document.getElementById('villageName-error').style.display = 'block';
-//
-//        return false;
-//    }
-//
-//    return true;
-//}
 function validateForm() {
     var isVillageNameValid = validateVillageName();
     var isRegionValid = validateRegion();
- resetErrorMessages();
-    // Check if both inputs are valid
+
     if (!isRegionValid || !isVillageNameValid ) {
     console.log("1 if ")
-        // Display error messages
+
         if (!isVillageNameValid) {
         console.log("village " + isVillageNameValid)
          document.getElementById('villageName-error').style.display = 'block';
-//            displayErrorMessage('villageName-error', 'Please enter a village name.');
+
         }
-        if (!isRegionValid.value) {
+        if (!isRegionValid) {
         console.log("region " + isRegionValid)
         document.getElementById('regionName-error').style.display = 'block';
-//            displayErrorMessage('regionName-error', 'region.error.message');
+
         }
         return false;
     }
 
     return true;
 }
-//function displayErrorMessage(elementId, message) {
-//    // Display error message for the specified element
-//    var errorElement = document.getElementById(elementId);
-//    if (errorElement) {
-//        errorElement.textContent = message;
-//        errorElement.style.display = 'block';
-//    }
-//}
+
   function validateRegion() {
         var regionSelect = document.getElementById('regionName');
 
@@ -63,13 +38,7 @@ function validateForm() {
             return true;
         }
     }
-function resetErrorMessages() {
-    // Hide all error messages
-    var errorElements = document.querySelectorAll('.regionName-error');
-    errorElements.forEach(function(element) {
-        element.style.display = 'none';
-    });
-}
+
     function validateVillageName() {
         var villageNameInput = document.getElementById('villageName');
         var villageName = villageNameInput.value.trim();
