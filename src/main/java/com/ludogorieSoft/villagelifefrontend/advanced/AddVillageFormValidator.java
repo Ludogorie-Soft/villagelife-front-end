c19155b3a261cfc9800a9406f8f12b3c4699ffd9
Nadezhda Todorova <ntodor358@gmail.com>
2024-04-13 21:55:47 +0300

    translate validations in add-village.html

--- src/main/java/com/ludogorieSoft/villagelifefrontend/advanced/AddVillageFormValidator.java
+++ src/main/java/com/ludogorieSoft/villagelifefrontend/advanced/AddVillageFormValidator.java
@@ -27,32 +27,32 @@ public class AddVillageFormValidator implements Validator {
         AddVillageFormResult addVillageFormResult = (AddVillageFormResult) target;
 
         if (addVillageFormResult.getVillageDTO().getName().trim().equals("")) {
-            errors.rejectValue("villageDTO.name", FIELD_REQUIRED, "Enter village name!");
+            errors.rejectValue("villageDTO.name", FIELD_REQUIRED, "error.enterVillageName");
         }/* else if (Boolean.FALSE.equals(validationUtilsClient.usernameCheck(addVillageFormResult.getVillageDTO().getName()))) { //method already check for cyrillic and latin names
-            errors.rejectValue("villageDTO.name", FIELD_REQUIRED, "Must use letters only!");
+            errors.rejectValue("villageDTO.name", FIELD_REQUIRED, "error.useLettersOnly");
         }*/
 
         if(Boolean.FALSE.equals(regionClient.existsRegionByName(addVillageFormResult.getVillageDTO().getRegion()))){
-            errors.rejectValue("villageDTO.region", FIELD_REQUIRED, "You must choose a region!");
+            errors.rejectValue("villageDTO.region", FIELD_REQUIRED, "error.chooseRegion");
         }
 
         if (addVillageFormResult.getEthnicityDTOIds() == null) {
-            errors.rejectValue("ethnicityDTOIds", FIELD_REQUIRED, "Choose at least one ethnicity group!");
+            errors.rejectValue("ethnicityDTOIds", FIELD_REQUIRED, "error.chooseEthnicityGroup");
         }
         if (addVillageFormResult.getGroundCategoryIds() == null) {
-            errors.rejectValue("groundCategoryIds", FIELD_REQUIRED, "Choose at least one ground category");
+            errors.rejectValue("groundCategoryIds", FIELD_REQUIRED, "error.chooseGroundCategory");
         }
         if (addVillageFormResult.getPopulationDTO().getPopulationCount() < 1) {
-            errors.rejectValue("populationDTO.populationCount", "field.minlength", "The population count must be greater than or equal to 1!");
+            errors.rejectValue("populationDTO.populationCount", "field.minlength", "error.populationCountMin");
         }
         if (addVillageFormResult.getPopulationDTO().getResidents() == null) {
-            errors.rejectValue("populationDTO.residents", FIELD_REQUIRED, "Select what percentage of the village residents are people under 50 years old!");
+            errors.rejectValue("populationDTO.residents", FIELD_REQUIRED, "error.selectPercentageUnder50");
         }
         if (addVillageFormResult.getPopulationDTO().getChildren() == null) {
-            errors.rejectValue("populationDTO.children", FIELD_REQUIRED, "Select the number of children (up to 14 years old)!");
+            errors.rejectValue("populationDTO.children", FIELD_REQUIRED, "error.selectNumberOfChildren");
         }
         if (addVillageFormResult.getPopulationDTO().getForeigners() == null) {
-            errors.rejectValue("populationDTO.foreigners", FIELD_REQUIRED, "Answer whether there are foreigners living in the village!");
+            errors.rejectValue("populationDTO.foreigners", FIELD_REQUIRED, "error.answerForeignersLiving");
         }
     }
 }
