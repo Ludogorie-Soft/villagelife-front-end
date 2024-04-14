18dba7ec963811e932395f213b41f6712ba59a37
Nadezhda Todorova <ntodor358@gmail.com>
2024-04-14 18:04:04 +0300

    translate validation for invalid image format

--- src/main/resources/static/js/validateImageFormat.js
+++
@@ -1,8 +0,0 @@
-function validateImageFormat(input) {
-     var file = input.files[0];
-     var allowedFormats = ["image/jpeg", "image/png"]
-     if (!allowedFormats.includes(file.type)) {
-         alert("Invalid format. Only JPG and PNG are allowed.");
-         input.value = '';
-     }
- }
