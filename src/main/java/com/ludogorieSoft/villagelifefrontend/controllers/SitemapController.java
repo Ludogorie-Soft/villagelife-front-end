fb33e71b70c3450748c6eed86d63e7e81e5e80b3
Nadezhda Todorova <ntodor358@gmail.com>
2024-07-17 19:07:25 +0300

    edit getSitemap()

--- src/main/java/com/ludogorieSoft/villagelifefrontend/controllers/SitemapController.java
+++ src/main/java/com/ludogorieSoft/villagelifefrontend/controllers/SitemapController.java
@@ -3,13 +3,17 @@ package com.ludogorieSoft.villagelifefrontend.controllers;
 import com.ludogorieSoft.villagelifefrontend.utils.SitemapGenerator;
 import lombok.AllArgsConstructor;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 
 @Controller
@@ -18,16 +22,23 @@ public class SitemapController {
     private final SitemapGenerator sitemapGenerator;
 
     @GetMapping("/sitemap.xml")
-    public ResponseEntity<Resource> getSitemap() throws MalformedURLException {
-        sitemapGenerator.createSitemap();
-
-        Resource resource = new ClassPathResource("/opt/villagelife/villagelife-front-end/target/src/main/resources/static/sitemap.xml");
-        System.out.println("Exists: " + resource.exists());
-
-        return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"sitemap.xml\"")
-                .contentType(MediaType.APPLICATION_XML)
-                .body(resource);
+    public ResponseEntity<FileSystemResource> getSitemap() {
+        try {
+            // Generate the sitemap
+            sitemapGenerator.createSitemap();
+            // Path to the generated sitemap in the target directory
+            File sitemapFile = new File("target/src/main/resources/static/sitemap.xml");
+            if (!sitemapFile.exists()) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+            FileSystemResource resource = new FileSystemResource(sitemapFile);
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"sitemap.xml\"")
+                    .contentType(MediaType.APPLICATION_XML)
+                    .body(resource);
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
 
