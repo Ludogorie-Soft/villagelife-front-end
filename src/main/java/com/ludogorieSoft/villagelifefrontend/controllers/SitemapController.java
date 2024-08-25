6dc7d9c9fa46e187858397c5e77165b60874f957
diddka <danailova_rz@abv.bg>
2024-08-25 20:56:07 +0300

    generatе a sitemap in xml format

--- src/main/java/com/ludogorieSoft/villagelifefrontend/controllers/SitemapController.java
+++ src/main/java/com/ludogorieSoft/villagelifefrontend/controllers/SitemapController.java
@@ -4,7 +4,6 @@ import com.ludogorieSoft.villagelifefrontend.utils.SitemapGenerator;
 import com.ludogorieSoft.villagelifefrontend.utils.XmlUrlSet;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
