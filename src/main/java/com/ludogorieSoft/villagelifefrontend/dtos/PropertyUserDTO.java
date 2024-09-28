ee016ac84de2c347307aebe99f7e3a7c289eee1d
Nadezhda Todorova <ntodor358@gmail.com>
2024-09-28 21:27:14 +0300

    add DTOs

---
+++ src/main/java/com/ludogorieSoft/villagelifefrontend/dtos/PropertyUserDTO.java
@@ -0,0 +1,42 @@
+package com.ludogorieSoft.villagelifefrontend.dtos;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.ludogorieSoft.villagelifefrontend.enums.OwnershipType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PropertyUserDTO {
+    private Long id;
+    private OwnershipType ownershipType;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    //Todo add access code or link
+    private UserSearchDataDTO userSearchDataDTO;
+    private String password;
+    private String jobTitle;
+    private BusinessCardDTO businessCardDTO;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    private LocalDateTime createdAt;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    private LocalDateTime deletedAt;
+}
