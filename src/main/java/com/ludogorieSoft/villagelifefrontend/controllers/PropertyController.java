6d3d8d8db6c06c0574a822ec10d4b75ad339ab5c
atche1 <atche1202@gmail.com>
2024-10-15 21:33:02 +0300

    add heating types

--- src/main/java/com/ludogorieSoft/villagelifefrontend/controllers/PropertyController.java
+++ src/main/java/com/ludogorieSoft/villagelifefrontend/controllers/PropertyController.java
@@ -29,13 +29,11 @@ import java.util.stream.Collectors;
 public class PropertyController {
     private PropertyClient propertyClient;
     private VillageClient villageClient;
-    private PropertyImageClient propertyImageClient;
     private static final String PROPERTY_DTO_NAME = "propertyDTO";
     private static final String PROPERTY_SAVE = "/save";
     private static final String REDIRECT_INDEX = "redirect:/";
     private static final String ERROR_MESSAGE = "errorMessage";
     private static final String PERMISSIONS_MESSAGE = "You do not have permissions for this page!";
-    private static final long MAX_FILE_SIZE = (long) 5 * 1024 * 1024;
 
     @GetMapping(value = {"/{page}", ""})
     String listProperties(Model model, @PathVariable(name = "page", required = false) Integer page) {
@@ -52,50 +50,6 @@ public class PropertyController {
         }
         return "/property/create-property";
     }
-
-    //    @PostMapping(PROPERTY_SAVE)
-//    public String submitProperty(@Valid @ModelAttribute("propertyDTO") PropertyDTO propertyDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes, HttpSession session){
-//        try {
-//            if (bindingResult.hasErrors()) {
-//                bindingResult.getAllErrors().forEach(error -> {
-//                    System.out.println("Error: " + error.getObjectName() + " - " + error.getDefaultMessage());
-//                });
-//                redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.propertyDTO", bindingResult);
-//                redirectAttributes.addFlashAttribute(PROPERTY_DTO_NAME, propertyDTO);
-//                return "redirect:/properties/add";
-//            }
-//            VillageDTO villageDTO = villageClient.findVillageByNameAndRegion(propertyDTO.getVillageDTO().getName() + ", " + propertyDTO.getVillageDTO().getRegion());
-//            if (villageDTO == null) {
-//                redirectAttributes.addFlashAttribute(ERROR_MESSAGE, "Селището не е намерено.");
-//                redirectAttributes.addFlashAttribute(PROPERTY_DTO_NAME, propertyDTO);
-//                return "redirect:/properties/add";
-//            }
-//            propertyDTO.setVillageDTO(villageDTO);
-//            propertyDTO.setPropertyUserDTO(getLoggedPropertyUserDTO(session));
-//            List<byte[]> imageBytes = new ArrayList<>();
-//            if (images.get(0).getSize() > 0) {
-//
-//               if (bindingResult.hasErrors()) {
-//                   redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.propertyDTO", bindingResult);
-//                   redirectAttributes.addFlashAttribute(PROPERTY_DTO_NAME, propertyDTO);
-//                   return "redirect:/properties/add";
-//               }
-//               hasExceededFileSize(images);
-//              convertImagesToBytes(images, imageBytes);
-//           }
-//           propertyDTO.setImageBytes(imageBytes);
-//            propertyClient.createProperty(propertyDTO);
-//           // propertyImageClient.createPropertyImage(propertyDTO.getImages());
-//            return "redirect:/properties";
-//        } catch (FeignException exception) {
-//            if (exception.status() == 401) {
-//                redirectAttributes.addFlashAttribute(ERROR_MESSAGE,
-//                        PERMISSIONS_MESSAGE);
-//                return REDIRECT_INDEX;
-//            }
-//        }
-//        return "redirect:/properties";
-//    }
     @PostMapping(PROPERTY_SAVE)
     public String submitProperty(@Valid @ModelAttribute("propertyDTO") PropertyDTO propertyDTO, @RequestParam("mainImage") MultipartFile mainImage, @RequestParam("propertyImages") List<MultipartFile> propertyImages, BindingResult bindingResult, RedirectAttributes redirectAttributes, HttpSession session) {
         if (bindingResult.hasErrors()) {
@@ -131,15 +85,6 @@ public class PropertyController {
         return "redirect:/properties";
     }
 
-    private void hasExceededFileSize(List<MultipartFile> images) throws ImageMaxUploadSizeExceededException {
-        long totalSize = images.stream()
-                .mapToLong(MultipartFile::getSize)
-                .sum();
-        if (totalSize > PropertyController.MAX_FILE_SIZE) {
-            throw new ImageMaxUploadSizeExceededException("File size should not exceed 5 MB");
-        }
-    }
-
     private byte[] convertImageToBytes(MultipartFile image) {
         byte[] imageData = null;
         if (image.getSize() > 0) {
@@ -152,21 +97,6 @@ public class PropertyController {
 
         return imageData;
     }
-//    private List<byte[]> convertImagesToBytes(List<MultipartFile> images) {
-//        return images.stream()
-//                .filter(image -> image != null && image.getSize() > 0)
-//                .map(image -> {
-//                    try {
-//                        return image.getBytes();
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                        return null;
-//                    }
-//                })
-//                .filter(bytes -> bytes != null)
-//                .toList();
-//    }
-
 
     public PropertyUserDTO getLoggedPropertyUserDTO(HttpSession session) {
         PropertyUserDTO loggedPropertyUserDTO = (PropertyUserDTO) session.getAttribute("info");
--- src/main/java/com/ludogorieSoft/villagelifefrontend/dtos/PropertyDTO.java
+++ src/main/java/com/ludogorieSoft/villagelifefrontend/dtos/PropertyDTO.java
@@ -39,6 +39,7 @@ public class PropertyDTO {
     private OwnershipType ownershipType;
     private PropertyCondition propertyCondition;
     private byte[] mainImageBytes;
+    private String heatingText;
 
     @JsonSerialize(using = LocalDateTimeSerializer.class)
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
