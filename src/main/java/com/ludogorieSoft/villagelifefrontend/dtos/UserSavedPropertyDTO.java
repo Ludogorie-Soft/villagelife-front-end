053de3fa8aa7a78cee75bfb7737582afad24e087
Nadezhda Todorova <ntodor358@gmail.com>
2024-10-29 08:45:54 +0200

    show businessCard in property.html

--- src/main/java/com/ludogorieSoft/villagelifefrontend/dtos/UserSavedPropertyDTO.java
+++ src/main/java/com/ludogorieSoft/villagelifefrontend/dtos/UserSavedPropertyDTO.java
@@ -18,7 +18,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class UserSavedPropertyDTO {
     private Long id;
-    private PropertyUserDTO userDTO;
+    private AlternativeUserDTO alternativeUserDTO;
     private PropertyDTO propertyDTO;
 
     @JsonSerialize(using = LocalDateTimeSerializer.class)
