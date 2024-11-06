6b8669beb0de9024ef2c66aef5daf0e49e2b87ba
Nadezhda Todorova <ntodor358@gmail.com>
2024-11-06 20:21:37 +0200

    add toggleUserSavedProperty

--- src/main/java/com/ludogorieSoft/villagelifefrontend/controllers/PropertyController.java
+++ src/main/java/com/ludogorieSoft/villagelifefrontend/controllers/PropertyController.java
@@ -2,6 +2,8 @@ package com.ludogorieSoft.villagelifefrontend.controllers;
 
 import com.ludogorieSoft.villagelifefrontend.config.PropertyClient;
 import com.ludogorieSoft.villagelifefrontend.config.PropertyImageClient;
+import com.ludogorieSoft.villagelifefrontend.config.UserSavedPropertyClient;
+import com.ludogorieSoft.villagelifefrontend.dtos.AlternativeUserDTO;
 import com.ludogorieSoft.villagelifefrontend.dtos.PropertyDTO;
 import com.ludogorieSoft.villagelifefrontend.dtos.PropertyImageDTO;
 import com.ludogorieSoft.villagelifefrontend.dtos.SubscriptionDTO;
@@ -10,8 +12,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -20,6 +25,7 @@ import java.util.List;
 public class PropertyController {
     private PropertyClient propertyClient;
     private PropertyImageClient propertyImageClient;
+    private UserSavedPropertyClient userSavedPropertyClient;
     private static final String SUBSCRIPTION_ATTRIBUTE = "subscription";
 
     @GetMapping(value = {"/{page}", ""})
@@ -32,13 +38,37 @@ public class PropertyController {
     }
 
     @GetMapping("/show/{id}")
-    public String showPropertyById(@PathVariable(name = "id") Long id, Model model) {
+    public String showPropertyById(@PathVariable(name = "id") Long id, Model model, HttpSession session) {
         PropertyDTO propertyDTO = propertyClient.getPropertyWithMainImageById(id);
         List<PropertyImageDTO> propertyImageDTOs = propertyImageClient.getAllPropertyImagesByPropertyId(id);
         propertyImageDTOs.add(new PropertyImageDTO(null, propertyDTO.getImageUrl(), null, null));
+
         model.addAttribute("property", propertyDTO);
         model.addAttribute("propertyImages", propertyImageDTOs);
         model.addAttribute(SUBSCRIPTION_ATTRIBUTE, new SubscriptionDTO());
+
+        AlternativeUserDTO loggedUser = (AlternativeUserDTO) session.getAttribute("info");
+        boolean hasSaved = false;
+        if (loggedUser != null) {
+            hasSaved = userSavedPropertyClient.isPropertySavedByPropertyIdAndAlternativeUserId(id, loggedUser.getId());
+        }
+        model.addAttribute("loggedUser", loggedUser);
+        model.addAttribute("hasSaved", hasSaved);
+
         return "/property/property";
     }
+
+    @PostMapping("/toggle-user-saved-property/{propertyId}")
+    public String toggleUserSavedProperty(@PathVariable("propertyId") Long propertyId, HttpSession session, RedirectAttributes redirectAttributes) {
+        AlternativeUserDTO loggedUser = (AlternativeUserDTO) session.getAttribute("info");
+
+        if (loggedUser == null) {
+            redirectAttributes.addFlashAttribute("loginModal", true);
+            return "redirect:/properties/show/" + propertyId;
+        }
+
+        userSavedPropertyClient.togglePropertySavedByPropertyIdAndAlternativeUserId(propertyId, loggedUser.getId());
+        return "redirect:/properties/show/" + propertyId;
+    }
+
 }
