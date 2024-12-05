89d861be7866f7a3fe5fd5923cd44fc3f61ecddd
Nadezhda Todorova <ntodor358@gmail.com>
2024-12-05 21:49:04 +0200

    add incrementSearchPropertiesSeenInResults

--- src/main/java/com/ludogorieSoft/villagelifefrontend/config/PropertyStatsClient.java
+++ src/main/java/com/ludogorieSoft/villagelifefrontend/config/PropertyStatsClient.java
@@ -2,10 +2,8 @@ package com.ludogorieSoft.villagelifefrontend.config;
 
 import com.ludogorieSoft.villagelifefrontend.dtos.PropertyStatsDTO;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+
 
 @FeignClient(name = "villagelife-api-property-stats",url = "${backend.url}/property-stats")
 public interface PropertyStatsClient {
--- src/main/java/com/ludogorieSoft/villagelifefrontend/controllers/PropertyStatsController.java
+++ src/main/java/com/ludogorieSoft/villagelifefrontend/controllers/PropertyStatsController.java
@@ -3,9 +3,7 @@ package com.ludogorieSoft.villagelifefrontend.controllers;
 import com.ludogorieSoft.villagelifefrontend.config.PropertyStatsClient;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @AllArgsConstructor
