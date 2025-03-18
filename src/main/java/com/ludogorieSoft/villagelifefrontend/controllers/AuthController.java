4917e23cb5fef25a1b3d36c0147f3a342e394c21
TodorYanakiev <todoryanakiev395@gmail.com>
2025-03-18 09:33:44 +0200

    Integrate recaptcha

--- src/main/java/com/ludogorieSoft/villagelifefrontend/controllers/AuthController.java
+++ src/main/java/com/ludogorieSoft/villagelifefrontend/controllers/AuthController.java
@@ -53,6 +53,7 @@ public class AuthController {
     private static final String RESET_PASSWORD_REQUEST = "resetPasswordRequest";
     private static final String USER_EMAIL = "userEmail";
     private static final String VERIFICATION_REQUEST = "verificationRequest";
+    private static final String RECAPTCHA_ERROR = "reCaptchaError";
     private final AuthClient authClient;
     private final BusinessCardDTOValidator businessCardDTOValidator;
 
@@ -99,7 +100,14 @@ public class AuthController {
         } catch (DuplicateEmailException ex) {
             checkDuplicateEmailException(ex, redirectAttributes, request);
         } catch (ApiRequestException e) {
-            if (e.getMessage().equals("Email already used!"))
+            String message = e.getMessage();
+            if (message.contains("Client exceeded maximum number of failed attempts"))
+                redirectAttributes.addFlashAttribute(RECAPTCHA_ERROR, "register.recaptcha.exceeded.attempts");
+            if (message.contains("Response contains invalid characters") || message.contains("reCaptcha was not successfully validated"))
+                redirectAttributes.addFlashAttribute(RECAPTCHA_ERROR, "register.recaptcha.invalid");
+            if (message.contains("Registration unavailable at this time.  Please try again later."))
+                redirectAttributes.addFlashAttribute(RECAPTCHA_ERROR, "register.recaptcha.unavailable");
+            if (message.equals("Email already used!"))
                 redirectAttributes.addFlashAttribute("duplicateBusinessEmailError", "business.card.validations.email.duplicate");
         }
         redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.adminNew", bindingResult);
